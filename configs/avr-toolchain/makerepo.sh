22f24cb093535d713721b199c5c7f63f7df45847
Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>
2010-03-14 05:30:04 -0300

    Add simple makerepo.sh for custom repo (needs more work)
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>

---
+++ configs/avr-toolchain/makerepo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+REPO_ARCH_AVR=/home/arch-avr/os/$(uname -m)
+
+mkdir -p $REPO_ARCH_AVR
+mkdir repo
+cd repo
+
+wget http://aur.archlinux.org/packages/nbd/nbd.tar.gz
+tar xf nbd.tar.gz
+cd nbd
+makepkg --asroot
+cp nbd*pkg* $REPO_ARCH_AVR
+
+repo-add $REPO_ARCH_AVR/arch-avr.db.tar.gz $REPO_ARCH_AVR/*pkg*
