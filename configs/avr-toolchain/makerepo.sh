268afde1a0a8095a24b96498f4015ec2c39ebaa3
Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>
2010-03-18 01:55:15 -0300

    [avr-toolchain] Done all modifications needed for changes in pxe_nbd branch
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>

--- configs/avr-toolchain/makerepo.sh
+++ configs/avr-toolchain/makerepo.sh
@@ -6,10 +6,10 @@ mkdir -p $REPO_ARCH_AVR
 mkdir repo
 cd repo
 
-wget http://aur.archlinux.org/packages/nbd/nbd.tar.gz
-tar xf nbd.tar.gz
-cd nbd
-makepkg --asroot
-cp nbd*pkg* $REPO_ARCH_AVR
+# wget http://aur.archlinux.org/packages/nbd/nbd.tar.gz
+# tar xf nbd.tar.gz
+# cd nbd
+# makepkg --asroot
+# cp nbd*pkg* $REPO_ARCH_AVR
 
 repo-add $REPO_ARCH_AVR/arch-avr.db.tar.gz $REPO_ARCH_AVR/*pkg*
